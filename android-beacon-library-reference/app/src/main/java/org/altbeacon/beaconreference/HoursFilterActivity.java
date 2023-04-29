801d3ab68873b14a8766e4205c70f2eb85ef00d0
javi-11 <javiterrobaorozco@gmail.com>
2023-04-29 22:33:55 +0200

    Arreglo de errores de beacons y adición de la funcionalidad de filtrado por día y hora

---
+++ android-beacon-library-reference/app/src/main/java/org/altbeacon/beaconreference/HoursFilterActivity.java
@@ -0,0 +1,14 @@
+package org.altbeacon.beaconreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoursFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hours_filter);
+    }
+}
